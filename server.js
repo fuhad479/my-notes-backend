2ca37e92f8a20b5a6450318060604e05c4afa251
fuhad479 <fuhadhasan479@gmail.com>
2023-07-06 13:31:25 +0600

    added cors middleware

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from "dotenv";
 import express from "express";
 
@@ -5,10 +6,11 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.status(200).json({ message: "ready to rock🚀🚀" });
 });
 
